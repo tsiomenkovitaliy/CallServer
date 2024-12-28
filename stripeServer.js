a5be541374e4f8192c69736eb8bece96e3bd1025
tsiomenkovitaliy <74013786+tsiomenkovitaliy@users.noreply.github.com>
2024-12-28 18:47:47 +0200

    fix

--- stripeServer.js
+++ stripeServer.js
@@ -4,13 +4,10 @@ const http = require('http');
 const { Server } = require('socket.io');
 const mongoose = require('mongoose');
 const { v4: uuidv4 } = require('uuid');
-const User = require('./models/User'); // Убедитесь, что путь к модели верный
+const User = require('./User'); // Убедитесь, что путь к модели верный
 
 // Подключение к MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/chatApp', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect('mongodb://127.0.0.1:27017/chatApp')
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
